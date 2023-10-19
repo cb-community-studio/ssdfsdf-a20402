b1804b00f120ab350eb24c17189f768a4be098c0
kanacb <kana@cloudbasha.com>
2023-10-19 23:08:13 +0800

    CodeBridge commit

---
+++ nodejs-backend/src/services/index.js
@@ -0,0 +1,7 @@
+// ~cb-add-require-service-name~
+
+// eslint-disable-next-line no-unused-vars
+module.exports = function (app) {
+    app.configure(users);
+    // ~cb-add-configure-service-name~
+};
---
+++ react-frontend/src/MyRouter/MyRouter.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import ProtectedRoute from './ProtectedRoute';
+import NoMatch from './NoMatch';
+
+import LoginPage from '../components/LoginPage/LoginPage';
+import SignUpPage from '../components/LoginPage/SignUpPage';
+import Account from '../components/Account/Account';
+import Dashboard from '../components/Dashboard/Dashboard';
+
+// ~cb-add-import~
+
+const MyRouter = () => {
+    return (
+        <Routes>
+            <Route path="" exact element={<Dashboard />} />
+            <Route path="/dashboard" exact element={<Dashboard />} />
+            <Route path="/login" exact element={<LoginPage />} />
+            <Route path="/signup" exact element={<SignUpPage />} />
+            {/* protected route https://www.robinwieruch.de/react-router-private-routes/ */}
+
+            <Route element={<ProtectedRoute redirectPath={'/login'} />}>
+                <Route path="/account" exact element={<Account />} />
+                {/* ~cb-add-protected-route~ */}
+            </Route>
+            {/* ~cb-add-route~ */}
+
+            <Route path="*" element={<NoMatch />} />
+        </Routes>
+    );
+};
+
+export default MyRouter;
